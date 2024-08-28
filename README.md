76a898039db365a114f50f8a9ae82040cce81f22
AbdullahKadad <abdullah.qdad.2@gmail.com>
2024-08-28 19:33:04 +0300

    dep4

--- README.md
+++
@@ -1,1 +0,0 @@
-# BE-Cars-Django
--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
+#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
-# Install dependencies
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Convert static asset files
+python manage.py collectstatic --no-input
 
+# Apply any outstanding database migrations
+python manage.py migrate
 
-# Create a superuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'root@example.com', '1234')" | python manage.py shell
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
