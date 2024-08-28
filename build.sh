34a12209d969692bd3952966c1acd824096f126a
AbdullahKadad <abdullah.qdad.2@gmail.com>
2024-08-28 19:38:21 +0300

    dep5

--- build.sh
+++ build.sh
@@ -11,7 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python world_champ_2022/manage.py createsuperuser --no-input
-fi
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'root@example.com', 'Aa#20.1.25')" | python manage.py shell
